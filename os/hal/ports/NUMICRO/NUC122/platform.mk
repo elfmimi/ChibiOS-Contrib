5ad0e359c737b978ee579354362fb0f16007b7a3
Ein Terakawa <applause@elfmimi.jp>
2019-06-01 00:17:44 +0900

    NUC122: Fetch NUC122.h and patch

--- os/hal/ports/NUMICRO/NUC122/platform.mk
+++ os/hal/ports/NUMICRO/NUC122/platform.mk
@@ -30,3 +30,15 @@ PLATFORMINC = ${CHIBIOS}/os/hal/ports/common/ARMCMx \
               ${CHIBIOS_CONTRIB}/os/hal/ports/NUMICRO/NUC122
 
 
+# Fetch NUC122.h from github and apply the patch.
+NUC122_H = ${CHIBIOS_CONTRIB}/os/hal/ports/NUMICRO/NUC122/NUC122.h
+NUC122_H_URL = https://raw.githubusercontent.com/OpenNuvoton/NUC122BSP/master/Library/Device/Nuvoton/NUC122/Include/NUC122.h
+ifeq ($(filter clean,$(MAKECMDGOALS)),clean)
+$(info $(shell if [ -f ${NUC122_H} ]; then rm ${NUC122_H}; fi))
+else
+ifeq ($(wildcard ${NUC122_H}),)
+$(info fetching NUC122.h ...)
+$(info $(shell wget -O ${NUC122_H} ${NUC122_H_URL}))
+$(info $(shell cd $(dir ${NUC122_H}) && patch -p0 < NUC122.h.patch))
+endif
+endif
